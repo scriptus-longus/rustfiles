82e10d99de3d3f73dfbbcb7a625bc8a8cbf828a7
scriptus-longus <scriptus-longus@protonmail.com>
2024-09-20 15:30:17 +0200

    basic control for player

--- rustinvaders/src/main.rs
+++ rustinvaders/src/main.rs
@@ -1,23 +1,62 @@
 #![allow(dead_code, unused_variables)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 extern crate ncurses;
 
 use ncurses::*;
 
+pub struct Player {
+  x: i32,
+  y: i32
+}
+
 fn main() {
+
   initscr();
-
+  
   curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
-  //nodelay(stdscr(), true);
-
+  nodelay(stdscr(), true);
   noecho();
   cbreak();
 
-  addstr("Hello world").unwrap();
+  let mut width = 0i32;
+  let mut height = 0i32;
 
-  refresh();
+  getmaxyx(stdscr(), &mut height, &mut width);
+
+  let mut player = Player {x: width/2, y: height/2};
 
 
-  getch();
+  let mut ch = 0u8;
+
+  while ch != b'q' {
+    clear();
+
+    ch = getch() as u8;
+
+    
+    match ch {
+      b'j' =>
+      {
+        player.x -= 1;
+      }
+      b'k' =>
+      {
+        player.x += 1;
+      }
+      b' ' =>
+      {
+        mvprintw(0,0,"pew").unwrap();
+      }
+      _ => {}
+    } 
+
+    mvprintw(player.y, player.x, "#").unwrap();
+
+    refresh();
+    napms(80);
+  }
+
   endwin();
 
 }
