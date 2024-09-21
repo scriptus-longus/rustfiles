4d72462f76d2b0125fab1070fda93be352d286a6
scriptus-longus <scriptus-longus@protonmail.com>
2024-09-21 18:38:19 +0200

    added bullet collision detection

--- rustinvaders/src/main.rs
+++ rustinvaders/src/main.rs
@@ -59,7 +59,7 @@ fn main() {
   getmaxyx(stdscr(), &mut height, &mut width);
 
   let mut player = Player {x: width/2, y: height/2};
-  let invaders = vec![ Enemy {x: width/2, y: height/3, dir: Dir::dirRIGHT} ];
+  let mut invaders = vec![ Enemy {x: width/2, y: height/3, dir: Dir::dirRIGHT} ];
 
   let mut ch = 0u8;
 
@@ -82,7 +82,7 @@ fn main() {
       {
         bullets.push(Bullet {x: player.x, y: player.y-1});
       }
-      _ => {}
+      _ => {mvprintw(0,0, "hi").unwrap(); }
     } 
 
 
@@ -91,6 +91,7 @@ fn main() {
     for invader in invaders.iter() {
       mvprintw(invader.y, invader.x, "@").unwrap();
     }
+
     mvprintw(player.y, player.x, "#").unwrap();
 
     //let keep = bool[invaders.size()];
@@ -101,6 +102,27 @@ fn main() {
     }
 
     bullets.retain(|bullet| bullet.y > 0);
+
+    // rudementary collision detection
+    let mut retain_bullets = vec![true; bullets.len()]; 
+    let mut retain_invaders = vec![true; invaders.len()];   
+ 
+ 
+    for (i, bullet) in bullets.iter().enumerate() {
+      for (j, invader) in invaders.iter().enumerate() {
+        if invader.x == bullet.x && invader.y == bullet.y {
+          retain_bullets[i] = false;
+          retain_invaders[j] = false;
+        }
+      }
+    }
+
+   let mut iter = retain_bullets.iter();
+
+    bullets.retain(|_| *iter.next().unwrap());
+    iter = retain_invaders.iter();
+
+    invaders.retain(|_| *iter.next().unwrap());
 
     refresh();
     napms(80);
